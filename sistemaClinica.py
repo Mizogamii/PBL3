a583bf6690c1e7de3748aa306613997ccc41c5ec
Mizogamii <120470907+Mizogamii@users.noreply.github.com>
2024-01-04 05:46:00 -0300

    a

--- sistemaClinica.py
+++ sistemaClinica.py
@@ -1,3 +1,11 @@
+"""Autor: Sayumi Mizogami Santana
+Componente Curricular: EXA 854 - MI - Algoritmos
+ConcluÍdo em: INFORME A DATA DE CONCLUSÃO
+Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
+trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+do código, e estou ciente que estes trechos não serão considerados para fins de avaliação."""
 from datetime import datetime
 import json
 from classes import Paciente, Recepcao, MarcarHorarioPaciente
